ca2c401a80afe3bf5f6c7f72b3430d762f5f3bea
Ivahnn Garcia <migarcia@tip.edu.ph>
2023-11-20 13:16:33 +0800

    kaynabahala

--- resources/js/Pages/Busroutes/Edit.vue
+++ resources/js/Pages/Busroutes/Edit.vue
@@ -1,73 +1,78 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head,router  } from '@inertiajs/vue3';
-import { defineProps } from 'vue';
-import { reactive,ref } from 'vue'
-
-import PrimaryButton from '@/Components/PrimaryButton.vue'
+import { Head, router } from '@inertiajs/vue3';
+import { defineProps, reactive } from 'vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 import InputLabel from '@/Components/InputLabel.vue';
-import TextInput from '@/Components/TextInput.vue';
 
 const props = defineProps({
-      busroute: Object,
-      locations: Object
-  })
+  busroute: Object,
+  locations: Object,
+});
 
+const form = reactive({
+  _method: 'put',
+  origin: props.busroute.origin,
+  destination: props.busroute.destination,
+});
 
-
-  const form= reactive({
-            _method: 'put',
-            origin: props.busroute.origin,
-            destination: props.busroute.destination,
-    })
-
-    function update() {
-        router.post(`/busroute/${props.busroute.id}`, {
-            _method: 'put',
-            origin: form.origin,
-            destination: form.destination,
-        })
+const update = () => {
+  // Check if the origin and destination are the same
+  if (form.origin === form.destination) {
+    alert("Origin and destination can't be the same. Please select different locations.");
+    return;
   }
 
+  router.post(`/busroute/${props.busroute.id}`, {
+    _method: 'put',
+    origin: form.origin,
+    destination: form.destination,
+  });
+};
 </script>
 
-
-
 <template>
-    <Head title="Update Bus Route" />
+  <Head title="Update Bus Route" />
 
-    <AuthenticatedLayout>
-
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="overflow-hidden ma-8  bg-red-200 rounded-lg border shadow-xs">
-                    <div class="overflow-x-auto  m-8 ">
-                            <form @submit.prevent="update" >
-                                <div class="mb-4">
-                                    <InputLabel for="origin" class="block font-medium text-gray-700">
-                                        Select Origin:
-                                    </InputLabel>
-                                    <select id="route" v-model="form.origin" class="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:outline-none focus:border-indigo-500">
-                                        <option v-for="location in locations"  :value="location.location">
-                                            {{ location.location }} </option>
-                                   </select>
-                                </div>
-                                <div class="mb-4">
-                                    <InputLabel for="origin" class="block font-medium text-gray-700">
-                                        Select Origin:
-                                    </InputLabel>
-                                    <select id="route" v-model="form.destination" class="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:outline-none focus:border-indigo-500">
-                                        <option v-for="location in locations"  :value="location.location">
-                                            {{ location.location }} </option>
-                                   </select>
-                                </div>
-                                <PrimaryButton>Submit</PrimaryButton>
-                            </form>
-                    </div>
-                </div>
-            </div>
+  <AuthenticatedLayout>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="overflow-hidden ma-8 bg-red-200 rounded-lg border shadow-xs">
+          <div class="overflow-x-auto m-8">
+            <form @submit.prevent="update">
+              <div class="mb-4">
+                <InputLabel for="origin" class="block font-medium text-gray-700">
+                  Select Origin:
+                </InputLabel>
+                <select
+                  id="origin"
+                  v-model="form.origin"
+                  class="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:outline-none focus:border-indigo-500"
+                >
+                  <option v-for="location in locations" :key="location.id" :value="location.location">
+                    {{ location.location }}
+                  </option>
+                </select>
+              </div>
+              <div class="mb-4">
+                <InputLabel for="destination" class="block font-medium text-gray-700">
+                  Select Destination:
+                </InputLabel>
+                <select
+                  id="destination"
+                  v-model="form.destination"
+                  class="mt-1 block w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:outline-none focus:border-indigo-500"
+                >
+                  <option v-for="location in locations" :key="location.id" :value="location.location">
+                    {{ location.location }}
+                  </option>
+                </select>
+              </div>
+              <PrimaryButton>Submit</PrimaryButton>
+            </form>
+          </div>
         </div>
-
-    </AuthenticatedLayout>
+      </div>
+    </div>
+  </AuthenticatedLayout>
 </template>
--- resources/js/Pages/Locations/Edit.vue
+++ resources/js/Pages/Locations/Edit.vue
@@ -1,62 +1,66 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head,router  } from '@inertiajs/vue3';
+import { Head, router } from '@inertiajs/vue3';
 import { defineProps } from 'vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue'
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import InputLabel from '@/Components/InputLabel.vue';
-import { reactive,ref } from 'vue'
+import { reactive } from 'vue';
 
 const props = defineProps({
-      location: Object
-  })
+  location: Object,
+});
 
+const form = reactive({
+  _method: 'put',
+  location: props.location.location,
+});
 
-  const form= reactive({
-            _method: 'put',
-            location: props.location.location,
+function validateInput() {
+  // Allow only letters and spaces
+  const validChars = /^[a-zA-Z\s]*$/;
 
-
-    })
-
-    function update() {
-        router.post(`/location/${props.location.id}`, {
-            _method: 'put',
-            location: form.location,
-
-        })
+  if (!validChars.test(form.location)) {
+    alert("Invalid characters or numbers are not allowed.");
+    return false;
   }
 
+  return true;
+}
+
+function update() {
+  if (validateInput()) {
+    router.post(`/location/${props.location.id}`, {
+      _method: 'put',
+      location: form.location,
+    });
+  }
+}
 </script>
 
-
-
 <template>
-    <Head title="Update Location " />
+  <Head title="Update Location" />
 
-    <AuthenticatedLayout>
-
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="overflow-hidden ma-8 w-100  bg-red-200 rounded-lg border shadow-xs">
-                    <div class="overflow-x-auto  mx-8 ">
-                            <form @submit.prevent="update" >
-                                <InputLabel for="location" value="location"/>
-                                <TextInput
-                                    id="code"
-                                    type="text"
-                                    v-model="form.location"
-                                    required
-                                />
-                                <div class=" py-3 md:w-1/3">
-                                   <PrimaryButton>Submit</PrimaryButton>
-                                </div>
-                            </form>
-                    </div>
-                </div>
-            </div>
+  <AuthenticatedLayout>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="overflow-hidden ma-8 w-100 bg-red-200 rounded-lg border shadow-xs">
+          <div class="overflow-x-auto mx-8">
+            <form @submit.prevent="update">
+              <InputLabel for="location" value="Location" />
+              <TextInput
+                id="code"
+                type="text"
+                v-model="form.location"
+                required
+              />
+              <div class="py-3 md:w-1/3">
+                <PrimaryButton>Submit</PrimaryButton>
+              </div>
+            </form>
+          </div>
         </div>
-
-    </AuthenticatedLayout>
+      </div>
+    </div>
+  </AuthenticatedLayout>
 </template>
